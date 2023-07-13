3d9d4cf5ff96ea2a28af6b37e5c8c87abf10a77d
abhiugarg <abhiu.garg@revdau.com>
2023-07-13 21:47:08 +0530

    commit changes

--- main.go
+++ main.go
@@ -31,6 +31,12 @@ func main() {
 
 	switch deviceType := data["device.type"].(string); deviceType {
 		case "linux":
+			fmt.Println("Fetch the time while polling")
+			timestamp := sshconnection.ExtractTimestamp()
+			fmt.Printf("Timestamp: %s\n", timestamp)
+			fmt.Println("__________________________________")
+
+			fmt.Println("Extract CPU Percentage")
 			client, err := sshconnection.EstablishSSHConnection(host,port,username,password)
 			if err != nil {
 				fmt.Printf("Failed to establish SSH connection: %s\n", err)
@@ -38,19 +44,171 @@ func main() {
 			}
 			defer client.Close()
 
-			command := "top -bn1 | grep '%Cpu'"
-			output, err := sshconnection.ExecuteCommand(client, command)
+			// Extract cpu percentage
+			commandcpu := "top -bn1 | grep '%Cpu'"
+			outputcpu, err := sshconnection.ExecuteCommand(client, commandcpu)
 			if err != nil {
 				fmt.Printf("Failed to execute command: %s\n", err)
 				return
 			}
 
-			cpuPercentage, err := sshconnection.ExtractCPUPercentage(output)
+			cpuPercentage, err := sshconnection.ExtractCPUPercentage(outputcpu)
 			if err != nil {
 				fmt.Printf("Failed to extract CPU percentage: %s\n", err)
 				return
 			}
 			fmt.Printf("CPU Percentage: %s\n", cpuPercentage)
+            fmt.Println("____________________________________________")
+
+			// Extract os.name
+			fmt.Println("Extract osName")
+			commandos := "cat /etc/os-release"
+			outputos, err := sshconnection.ExecuteCommand(client, commandos)
+			if err != nil {
+				fmt.Printf("Failed to execute command: %s\n", err)
+				return
+			}
+
+			osName, err := sshconnection.ExtractOSName(outputos)
+			if err != nil {
+				fmt.Printf("Failed to extract OS Name: %s\n", err)
+				return
+			}
+			fmt.Printf("OS Name: %s\n", osName)
+			fmt.Println("_________________________________________________")
+
+			// Extract cpu.cores
+			fmt.Println("Extract the no. of cpu cores")
+			commandCpuCores := "cat /proc/cpuinfo | grep processor"
+			outputCpuCores, err := sshconnection.ExecuteCommand(client, commandCpuCores)
+			if err != nil {
+				fmt.Printf("Failed to execute command: %s\n", err)
+				return
+			}
+
+			noOfCpuCores, err := sshconnection.ExtractCPUCoreCount(outputCpuCores)
+			if err != nil {
+				fmt.Printf("Failed to extract CPU Core Count: %s\n", err)
+				return
+			}
+			fmt.Printf("CPU Core Count: %d\n", noOfCpuCores)
+			fmt.Println("_____________________________________")
+
+			// Error : Failed to execute command: Process exited with status 1
+			//Extract cpu.io.percent
+			// fmt.Println("Extract the CPU IO Percentage")
+			// commandIO := "iostat -c | grep '^avg-cpu'"
+			// outputIO, err := sshconnection.ExecuteCommand(client, commandIO)
+			// if err != nil {
+			// 	fmt.Printf("Failed to execute command: %s\n", err)
+			// 	return
+			// }
+
+			// cpuIOPercentage, err := sshconnection.ExtractCPUIOPercentage(outputIO)
+			// if err != nil {
+			// 	fmt.Printf("Failed to extract CPU IO Percentage: %s\n", err)
+			// 	return
+			// }
+			// fmt.Printf("CPU IO Percentage: %s\n", cpuIOPercentage)
+			// fmt.Println("___________________________________________")
+
+			// Extract memory used bytes
+			fmt.Println("Extract memory used in bytes")
+			commandMU := "free -b | grep Mem"
+			outputMU, err := sshconnection.ExecuteCommand(client, commandMU)
+			if err != nil {
+				fmt.Printf("Failed to execute command: %s\n", err)
+				return
+			}
+			
+			memoryUsed, err := sshconnection.ExtractMemoryUsed(outputMU)
+			if err != nil {
+				fmt.Printf("Failed to extract memory used: %s\n", err)
+				return
+			}
+			fmt.Printf("Memory Used: %d bytes\n", memoryUsed)
+			fmt.Println("___________________________________________")
+
+			
+			//Extract total memory in bytes
+			fmt.Println("Extract total memory in bytes")
+			//command is same that we use to fetch memory used
+			memoryTotal, err := sshconnection.ExtractMemoryTotal(outputMU)
+			if err != nil {
+				fmt.Printf("Failed to extract memory total: %s\n", err)
+				return
+			}
+			fmt.Printf("Memory Total: %d bytes\n", memoryTotal)
+			fmt.Println("___________________________________________")
+
+			// Extract memory used in percentage
+			fmt.Println("Extract memory used in percentage")
+			commandMUP := "free -m | grep Mem"
+			outputMUP, err := sshconnection.ExecuteCommand(client, commandMUP)
+			if err != nil {
+				fmt.Printf("Failed to execute command: %s\n", err)
+				return
+			}
+			
+			memoryUsedPercentage, err := sshconnection.ExtractMemoryUsedPercentage(outputMUP)
+			if err != nil {
+				fmt.Printf("Failed to extract memory used percentage: %s\n", err)
+				return
+			}
+			fmt.Printf("Memory Used Percentage: %s \n", memoryUsedPercentage)
+			fmt.Println("___________________________________________")
+
+			// Extract system.memory.used.percentage
+			fmt.Println("Extract system.memory.used.percentage")
+			command := "cat /proc/meminfo"
+			output, err := sshconnection.ExecuteCommand(client, command)
+			if err != nil {
+				fmt.Printf("Failed to execute command: %s\n", err)
+				return
+			}
+			
+			systemMemUsedPercentage, err := sshconnection.ExtractSystemMemoryUsedPercentage(output)
+			if err != nil {
+				fmt.Printf("Failed to extract system memory used percentage: %s\n", err)
+				return
+			}
+			fmt.Printf("System Memory Used Percentage: %s \n", systemMemUsedPercentage)
+			fmt.Println("___________________________________________")
+
+			//Fetch the interface name
+
+			fmt.Println("Fetch the interface name")
+			commInter := "ip link show"
+			//commInter := "ifconfig | grep 'interface.name'"
+			outputInterface, err := sshconnection.ExecuteCommand(client,commInter)
+			if err != nil {
+				fmt.Printf("Failed to execute command: %s\n", err)
+				return
+			}
+			interfaceName, err := sshconnection.ExtractInterfaceName(outputInterface)
+			if err != nil {
+				fmt.Printf("Failed to extract interface name : %s\n", err)
+				return
+			}
+			fmt.Printf("Interface Name: %s\n", interfaceName)
+			fmt.Println("________________________________________________")
+
+			// Fetch interface input speed in byte
+			fmt.Println("Fetch interface input speed in bytes")
+			commandIIS := "ip -s link show lo"
+			outputIIS, err := sshconnection.ExecuteCommand(client,commandIIS)
+			if err != nil {
+				fmt.Printf("Failed to execute command: %s\n", err)
+				return
+			}
+			inputSpeed, err := sshconnection.ExtractInterfaceInputSpeed(outputIIS)
+			if err != nil {
+				fmt.Printf("Failed to extract interface input speed: %s\n", err)
+				return
+			}
+			fmt.Printf("Interface Input Speed: %s\n", inputSpeed)
+
+			
 
 	    case "snmp":
 			
--- sshconnection/sshconnection.go
+++ sshconnection/sshconnection.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
+	"time"
 
 	"golang.org/x/crypto/ssh"
 )
+
+func ExtractTimestamp() string {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	return timestamp
+}
 
 func EstablishSSHConnection(serverAddr string, port int, username string, password string) (*ssh.Client, error) {
 
@@ -35,6 +42,7 @@ func ExecuteCommand(client *ssh.Client,command string) (string,error)  {
 
 	output,err := session.Output(command)
 	if err != nil {
+		fmt.Printf("Failed to execute command: %s. Error: %s\n", err, output)
 		return "",err
 	}
 	return string(output),nil
@@ -61,6 +69,187 @@ func ExtractCPUPercentage(output string) (string,error)  {
 	return fmt.Sprintf("%.2f", cpuPercentage), nil
 	
 }
+
+
+func ExtractOSName(output string) (string, error) {
+	pattern := `NAME="(.+)"` // Assuming the OS name is present in the NAME field of the output
+	re := regexp.MustCompile(pattern)
+	match := re.FindStringSubmatch(output)
+
+	fmt.Printf("Output: %s\n", output)
+
+	if len(match) != 2 {
+		return "", fmt.Errorf("unable to extract OS name")
+	}
+	osName := strings.TrimSpace(match[1])
+	return osName, nil
+}
+
+func ExtractCPUCoreCount(output string) (int, error) {
+	pattern := `processor\s+:\s+(\d+)`
+	re := regexp.MustCompile(pattern)
+	matches := re.FindAllStringSubmatch(output, -1)
+
+	fmt.Printf("Output: %s\n", output)
+
+	if len(matches) == 0 {
+		return 0, fmt.Errorf("unable to extract CPU core count")
+	}
+	coreCount := len(matches)
+	return coreCount, nil
+}
+
+func ExtractCPUIOPercentage(output string) (string, error) {
+	pattern := `IO:\s+(\d+\.\d+)\s+us`
+	re := regexp.MustCompile(pattern)
+	match := re.FindStringSubmatch(output)
+
+	fmt.Printf("Output: %s\n", output)
+
+	if len(match) != 2 {
+		return "", fmt.Errorf("unable to extract CPU IO percentage")
+	}
+	cpuIOPercentageStr := match[1]
+	cpuIOPercentage, err := strconv.ParseFloat(cpuIOPercentageStr, 64)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to convert CPU IO percentage")
+	}
+	return fmt.Sprintf("%.2f", cpuIOPercentage), nil
+}
+
+
+func ExtractMemoryUsed(output string) (int64, error) {
+	pattern := `Mem:\s+\d+\s+(\d+)\s+\d+`
+	re := regexp.MustCompile(pattern)
+	match := re.FindStringSubmatch(output)
+
+	fmt.Printf("Output: %s\n", output)
+	// fmt.Println(len(match))
+	// fmt.Println(match[0])
+	// fmt.Println(match[1])
+
+	if len(match) != 2 {
+		return 0, fmt.Errorf("unable to extract memory used")
+	}
+
+	memoryUsedStr := match[1]
+	memoryUsed, err := strconv.ParseInt(memoryUsedStr, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("failed to convert memory used")
+	}
+	return memoryUsed, nil
+}
+
+func ExtractMemoryTotal(output string) (int64,error)  {
+	pattern := `Mem:\s+(\d+)\s+`
+	re := regexp.MustCompile(pattern)
+	match := re.FindStringSubmatch(output)
+
+	fmt.Printf("Output: %s\n", output)
+	// fmt.Println(len(match))
+	// fmt.Println(match[0])
+	// fmt.Println(match[1])
+	
+	if len(match) != 2 {
+		return 0, fmt.Errorf("unable to extract memory total")
+	}
+	memoryTotalStr := match[1]
+	memoryTotal, err := strconv.ParseInt(memoryTotalStr, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("failed to convert memory total")
+	}
+	return memoryTotal, nil
+}
+
+func ExtractMemoryUsedPercentage(output string) (string, error) {
+	pattern := `Mem:\s+\d+\s+(\d+)\s+\d+`
+	re := regexp.MustCompile(pattern)
+	match := re.FindStringSubmatch(output)
+
+	fmt.Printf("Output: %s\n", output)
+
+	if len(match) != 2 {
+		return "", fmt.Errorf("unable to extract memory used percentage")
+	}
+
+	memoryUsedStr := strings.TrimSpace(match[1])
+	memoryUsed, err := strconv.ParseFloat(memoryUsedStr, 64)
+	if err != nil {
+		return "", fmt.Errorf("failed to convert memory used percentage")
+	}
+
+	return fmt.Sprintf("%.2f", memoryUsed), nil
+}
+
+func ExtractSystemMemoryUsedPercentage(output string) (string, error) {
+	pattern := `MemTotal:\s+(\d+) kB\n.*\nMemAvailable:\s+(\d+) kB`
+	re := regexp.MustCompile(pattern)
+	match := re.FindStringSubmatch(output)
+
+	fmt.Printf("Output: %s\n", output)
+	fmt.Println(len(match))
+	fmt.Println(match[0])
+	fmt.Println(match[1])
+	fmt.Println(match[2])
+
+	if len(match) != 3 {
+		return "", fmt.Errorf("unable to extract memory used percentage")
+	}
+
+	memTotalStr := match[1]
+	memAvailableStr := match[2]
+
+	memTotal, err := strconv.ParseFloat(memTotalStr, 64)
+	if err != nil {
+		return "", fmt.Errorf("failed to convert memory total")
+	}
+
+	memAvailable, err := strconv.ParseFloat(memAvailableStr, 64)
+	if err != nil {
+		return "", fmt.Errorf("failed to convert memory available")
+	}
+
+	memUsedPercentage := ((memTotal - memAvailable) / memTotal) * 100
+
+	return fmt.Sprintf("%.2f", memUsedPercentage), nil
+}
+
+func ExtractInterfaceName(output string) (string, error) {
+	pattern := `^\d+:\s+([^:@]+)`
+	//pattern := `^\d+:\s+([^\s:]+)`
+	re := regexp.MustCompile(pattern)
+	match := re.FindStringSubmatch(output)
+
+	fmt.Printf("Output: %s\n", output)
+	fmt.Println(match)
+	// fmt.Println(match[0])
+
+	if len(match) != 2 {
+		return "", fmt.Errorf("unable to extract interface name")
+	}
+	interfaceName := match[1]
+	return interfaceName, nil
+}
+
+func ExtractInterfaceInputSpeed(output string) (string, error) {
+	//fmt.Println(output)
+
+	lines := strings.Split(output, "\n")
+	for _, line := range lines {
+		if strings.Contains(line, "RX bytes:") {
+			fields := strings.Fields(line)
+			if len(fields) >= 3 {
+				inputSpeed := fields[2]
+				return inputSpeed, nil
+			}
+		}
+	}
+
+	return "", fmt.Errorf("unable to extract interface input speed")
+}
+
+
 
 // func main() {
 
